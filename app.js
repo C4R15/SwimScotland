8453b6f399be868d59e55c2587cfdcbb9922ddfb
C4R15 <chrispercevalmaxwell@gmail.com>
2017-06-15 22:55:13 +0100

    results started

--- app.js
+++ app.js
@@ -21,47 +21,46 @@ app.set('port', port);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use('/public', express.static(__dirname + '/public'));
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
 app.engine('hbs', hbs.express4({
-  defaultLayout: path.join(__dirname, '/views/layout.hbs'),
-  partialsDir: path.join(__dirname, '/views/partials')
-})
-)
+    defaultLayout: path.join(__dirname, '/views/layout.hbs'),
+    partialsDir: path.join(__dirname, '/views/partials')
+}));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 server.listen(port);
